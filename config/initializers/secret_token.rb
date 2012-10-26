ad2daf457731a99c4353c7afa04399f21ad4524f
Michael Kirby <mckgamer@yahoo.com>
2012-10-25 22:11:13 -0700

    * Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Telephoenix::Application.config.secret_token = 'ec29b038bf252df048f9853ee4a28e03762e9a806c00ab77010e6d3ccf1b74fc6dba3fb7d0c3cbe8ea23d246978e4a8e62b1bb55d251eb66c540897117375ce6'
