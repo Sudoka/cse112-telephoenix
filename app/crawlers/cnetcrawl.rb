59d0c0328ebdc2e8b9b0634759799309052f1191
Michael Kirby <mckgamer@yahoo.com>
2012-11-18 22:53:00 -0800

    mkirby: Updated CNET script to actual go through every phone, and pull user reviews as well closes #28.

--- app/crawlers/cnetcrawl.rb
+++ app/crawlers/cnetcrawl.rb
@@ -7,32 +7,56 @@ class CNETCrawl
   
   #Get the Source object from the DB
   source = Source.find_or_create_by_name(:cnet)
+  sourceU = Source.find_or_create_by_name(:cnetUser)
 
-  #Set and Load CNET URL
+  stillGoing = true
   url = "http://reviews.cnet.com/4566-6454_7-0-0.html?rpp=30"
-  doc = Nokogiri::HTML(open(url))
-  
-  #Scrape all phones and their ratings
-  info = doc.xpath('//div[@class="prodInfo"]')
-  
-  #Break down the data and add to DB
-  info.each do |i|
-    p = i.xpath('a/h3/text()[1]')
-    r = i.xpath('div/div/a/@data-tiptitle')
-    puts r.to_s
+  page = 1
+  while stillGoing
+    #Set and Load CNET URL
+    doc = Nokogiri::HTML(open(url))
+    
+    #Scrape all phones and their ratings
+    info = doc.xpath('//div[@class="prodInfo"]')
+    stillGoing = false
 
-    p = p.to_s.strip
-    brand = p[0,p.index(" ")]
-    name = p[p.index(" ")+1,p.length]
-    if (r.to_s.strip != "")
-      rate = Float(r.to_s[0,r.to_s.index(" ")])
+    #Break down the data and add to DB
+    info.each do |i|
+      stillGoing = true
+      p = i.xpath('a/h3/text()[1]')
+      r = i.xpath('div/div/a/@data-tiptitle')
+      userRev = i.xpath('div/span/a/@data-tiptitle')
+      numUserRev = i.xpath('div/span/a/@data-tiptext')
+      puts r.to_s
+      puts userRev.to_s + " with "+ numUserRev.to_s
+      
+      p = p.to_s.strip
+      brand = p[0,p.index(" ")]
+      name = p[p.index(" ")+1,p.length]
+      if (r.to_s.strip != "")
+        rate = Float(r.to_s[0,r.to_s.index(" ")])
+      end
+      if (userRev.to_s.strip != "")
+        userRate = Float(userRev.to_s[0,userRev.to_s.index(" ")])
+        temp = numUserRev.to_s
+        userRateNum = Integer(temp[5,temp[5, temp.length].index(" ")])
+      end
+      
+      
+      phone = Phone.find_or_create_by_name_and_brand(name, brand)
+      if (rate != nil)  
+        rating = Rating.find_or_create_by_source_id_and_phone_id(source.id, phone.id)
+        rating.rating = Integer(rate*20)
+        rating.save
+      end
+      if (userRate != nil)
+        userRating = Rating.find_or_create_by_source_id_and_phone_id(sourceU.id, phone.id)
+        userRating.rating = userRate*20
+        userRating.number_reviews = userRateNum
+        userRating.save
+      end
     end
-
-    phone = Phone.find_or_create_by_name_and_brand(name, brand)
-    if (rate != nil)  
-      rating = Rating.find_or_create_by_source_id_and_phone_id(source.id, phone.id)
-      rating.rating = Integer(rate*20)
-      rating.save
-    end
+    page = page+1
+    url = "http://reviews.cnet.com/4566-6454_7-0-"+page.to_s+".html?rpp=30"
   end
 end
