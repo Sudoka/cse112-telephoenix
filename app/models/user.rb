3356049fcc6265f0d7c6af457856f413ad0bdfe6
philb007 <ebbroitaliano22@gmail.com>
2012-11-18 22:01:31 -0800

    Deleted Moderator

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ require 'digest/sha1'
 
 class User < ActiveRecord::Base
   has_many :reviews
-  make_flag :flag_once => true
+  make_flagr
+  #make_flag :flag_once => true
   attr_accessible :username, :email, :password, :password_confirmation, :ip_address
   attr_protected :id, :salt
   attr_accessor :password, :password_confirmation
